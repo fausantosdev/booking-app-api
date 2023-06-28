666771c5a6f27a4ff5ad4f7c32c95c961492338b
fausantosdev <fausantosdev@hotmail.com>
2023-06-27 21:36:24 -0300

    :construction: fix: the `strictQuery` option will be switched back to `false` by default in Mongoose 7.

--- src/database/index.js
+++ src/database/index.js
@@ -6,6 +6,8 @@ class Database {
   }
 
   mongo() {
+    mongoose.set('strictQuery', false)
+    
     this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       //useFindAndModify: true,
